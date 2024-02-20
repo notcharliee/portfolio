a24561a7612b4eeedf7e2119dada038548a3d888
notcharliee <saunderscharlie@hotmail.com>
2024-02-20 23:20:23 +0000

    fix: add site to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 
 export default defineConfig({
+  site: "https://charliee.dev",
   integrations: [tailwind()],
   prefetch: true,
 })
